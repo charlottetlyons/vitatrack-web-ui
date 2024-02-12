aca8d0582e4ae227ed9056bf5e2bda1ed644bec6
Charlotte Lyons <charlottetlyons@gmail.com>
2024-02-11 19:47:54 -0600

    Finally change dropdown background color

--- src/components/inputs/DropDownTextInput.tsx
+++ src/components/inputs/DropDownTextInput.tsx
@@ -1,5 +1,18 @@
-import { Autocomplete, TextField, styled } from '@mui/material';
+import {
+    Autocomplete,
+    ListItem,
+    ListItemIcon,
+    Paper,
+    TextField,
+    styled,
+} from '@mui/material';
 import React from 'react';
+import CloseIcon from '@mui/icons-material/Close';
+
+const StyledDropDownBody = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.text.primary,
+    padding: '10px',
+}));
 
 const StyledAutocomplete = styled(Autocomplete)(({ theme }) => ({
     '& .MuiOutlinedInput-root': {
@@ -17,9 +30,19 @@ const DropDownTextInput: React.FC = () => {
     return (
         <StyledAutocomplete
             disablePortal
+            PaperComponent={StyledDropDownBody}
             options={['Banana', 'Grapes', 'Ears']}
             renderInput={(params) => (
                 <TextField {...params} placeholder="Food" />
+            )}
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            renderOption={(props, option: any) => (
+                <ListItem alignItems="center" {...props}>
+                    <ListItemIcon>
+                        <CloseIcon />
+                    </ListItemIcon>
+                    {option}
+                </ListItem>
             )}
         />
     );
