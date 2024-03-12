c0869c8c1a5ac62baacf9ff1b1c59fb79de588c7
Charlotte Lyons <charlottetlyons@gmail.com>
2024-03-11 20:09:37 -0500

    Added Rough Form Inputs

--- src/components/forms/AddFoodForm.tsx
+++ src/components/forms/AddFoodForm.tsx
@@ -1,10 +1,61 @@
-import { Dialog, Grid } from '@mui/material';
+import {
+    Dialog,
+    FormControlLabel,
+    Grid,
+    Radio,
+    RadioGroup,
+    Typography,
+} from '@mui/material';
 import React from 'react';
+import TextInput from '../inputs/TextInput';
+import PrimaryButton from '../buttons/PrimaryButton';
+import SecondaryButton from '../buttons/SecondaryButton';
 
 const AddFoodForm: React.FC = () => {
     return (
         <Dialog open={true}>
-            <Grid container>Food go in mouth</Grid>
+            <Grid container direction="column" alignItems="center" padding={5}>
+                <Grid item>
+                    <Typography variant="h2">Add Food</Typography>
+                </Grid>
+                <Grid item>
+                    <TextInput placeholderText="Name" />
+                    <Grid
+                        container
+                        direction="row"
+                        justifyContent="space-between"
+                    >
+                        <Grid item>
+                            <TextInput placeholderText="Serving Size" />
+                        </Grid>
+                        <Grid item>
+                            <TextInput placeholderText="Metric" />
+                        </Grid>
+                    </Grid>
+
+                    <TextInput placeholderText="Calories" />
+                    <TextInput placeholderText="Protein" />
+                    <TextInput placeholderText="Carbs" />
+                    <TextInput placeholderText="Fat" />
+                    <RadioGroup row>
+                        <FormControlLabel
+                            value="private"
+                            control={<Radio />}
+                            label="Private"
+                        />
+                        <FormControlLabel
+                            value="public"
+                            control={<Radio />}
+                            label="Public"
+                        />
+                    </RadioGroup>
+                    <SecondaryButton text="Add Image" />
+                </Grid>
+                <Grid item>
+                    <PrimaryButton text="Add" />
+                    <SecondaryButton text="Cancel" />
+                </Grid>
+            </Grid>
         </Dialog>
     );
 };
